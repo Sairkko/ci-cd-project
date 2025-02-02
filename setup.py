97e87988ad698ccf750b845fcecba5c605fb0e41
Sairkko <bastien.98@orange.fr>
2025-02-02 16:11:05 +0100

    fix: update ci + push tag

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ci_cd_project",
     version="0.1.0",
-    packages=find_packages(where="src"),  # <-- on indique que les sources sont dans src/
-    package_dir={"": "src"}
+    # Les sources se trouvent dans src/
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
