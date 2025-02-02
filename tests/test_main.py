33b5a8afcebf0c7f82a85faf74cc031eced63ae7
Sairkko <bastien.98@orange.fr>
2025-02-02 15:32:44 +0100

    Ajout des tests unitaires pour la fonction guess_the_word

---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+import pytest
+from src.main import guess_the_word
+
+def test_guess_correct():
+    assert guess_the_word("python") is True
+
+def test_guess_incorrect():
+    assert guess_the_word("java") is False
